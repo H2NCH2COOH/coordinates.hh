446c5a2d67aedf9ff52854863d3b66ea9c7dd44a
H2NCH2COOH <wzypublic@gmail.com>
2020-12-04 18:28:58 +0800

    Add a runtime compile optimization test

---
+++ runtime.cpp
@@ -0,0 +1,28 @@
+#if 0
+g++ -Wall -O2 -std=c++17 -S $0
+exit
+#endif
+
+#include <stdlib.h>
+#include "coordinates.hh"
+
+struct Left;
+struct Right;
+struct Top;
+struct Bottom;
+
+struct {} X;
+struct {} Y;
+
+using C1 = struct Coordinate<int, CoordinateAttr::Direction<Left, Right>, CoordinateAttr::Name<decltype(X)>>;
+using C2 = struct Coordinate<int, CoordinateAttr::Direction<Top, Bottom>, CoordinateAttr::Name<decltype(Y)>>;
+
+using V1 = struct Vec<C1, C2>;
+
+int main(int argc, char** argv) {
+   V1 v1(atoi(argv[1]), atoi(argv[2]));
+
+   v1 = v1 + V1(2, 1);
+
+   return v1[X].value;
+}
